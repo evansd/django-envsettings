66d29a015fd1daa2ee875f512433214ed43e88b2
David Evans <d@evans.io>
2014-12-07 11:59:40 +0000

    Packaging up

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+def read(*path):
+    full_path = os.path.join(PROJECT_ROOT, *path)
+    with codecs.open(full_path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='django-envsettings',
+    version='0.1',
+    author='David Evans',
+    author_email='d@evans.io',
+    url='http://github.com/evansd/django-envsettings',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description="One-stop shop for configuring 12 factor Django apps",
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
